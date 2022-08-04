667caa6b26340f7e5cbec03bac7ab8c71668565c
Burgerjoa <aikyan@kakao.com>
2022-08-04 19:57:55 +0900

    0804

---
+++ Assets/Scripts/Stats/CharStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    static int bbHP = 100;
+    public int bbDmg = 10;
+    public int bbAS = 5;
+    
+
+ 
+}
