f89f194fce59f818ae381d5e0ae9872ed20c6a7a
Burgerjoa <aikyan@kakao.com>
2022-09-05 04:03:36 +0900

    assets, camera move

--- Assets/Scripts/ReSpawn.cs
+++ Assets/Scripts/ReSpawn.cs
@@ -10,6 +10,9 @@ public class ReSpawn : MonoBehaviour
    void Start()
    {
     player = Instantiate(charPrefabs[(int)DataMgr.instance.currentCharacter]);
+    int index = player.name.IndexOf("(Clone)");
+    if (index > 0) 
+        player.name = player.name.Substring(0, index);
     player.transform.position = transform.position;
    }
 }
