2819b0c07a0a2a341e801f5ed3e2f11a0ac48a6e
KKM2222 <108340973+KKM2222@users.noreply.github.com>
2022-09-13 17:12:30 +0900

    Hp Bar

---
+++ Assets/Scripts/Game_play/UI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private Slider hpbar;
+
+    private float maxHp = 100;
+    private float curHp = 100;
+    float imsi;
+    void start()
+    {
+        hpbar.value = (float) curHp / (float) maxHp;
+    }
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(curHp > 0)
+            {
+                curHp -= 10;
+            }
+            else
+            {
+                curHp = 0;
+            }
+            imsi = (float)curHp / (float)maxHp;
+        }
+        HandleHp();
+    }
+
+    private void HandleHp()
+    {
+        hpbar.value = Mathf.Lerp(hpbar.value, (float) curHp / (float) maxHp, Time.deltaTime * 10) ;
+    }
+  
+}
