69ea1a71fd444cc83e8b5a85fdff962d4f4a8fc6
Burgerjoa <aikyan@kakao.com>
2022-11-08 11:21:51 +0900

    1234

---
+++ Assets/Scripts/ItemSysytem/GetItem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace ItemSysytem
+{
+    public class GetItem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+        
+        
+        void
+        
+        
+    }
+}
